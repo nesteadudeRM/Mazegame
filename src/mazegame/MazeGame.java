2548aaaac973a020711ffe3843158b4643be4232
nesteadudeRM <raun.mathur@gmail.com>
2015-08-27 20:14:19 -0400

    update

--- src/mazegame/MazeGame.java
+++ src/mazegame/MazeGame.java
@@ -45,42 +45,42 @@ public class MazeGame {
                
         BufferedReader br = new BufferedReader(new FileReader(layoutFileName));
         String line;
-        Banana B;
-        MobileBanana M;
+        Banana regularB;
+        MobileBanana mobileB;
         
-		for(int i=0; i<dimensions[0]; i++){ //loops through rows of file
+		for(int r=0; r<dimensions[0]; r++){ //loops through rows of file
 			line = br.readLine(); //creates String from a line in file
 
 			/*loops through columns of file
 			 * set the cells of grid with the following Sprites
 			*/
-			for(int j=0; j<dimensions[1]; j++){ 
-				if (line.charAt(j) == MazeConstants.WALL){
-					maze.setCell(i, j, new Wall(MazeConstants.WALL, i, j));
+			for(int c=0; c<dimensions[1]; c++){ 
+				if (line.charAt(c) == MazeConstants.WALL){
+					maze.setCell(r, c, new Wall(MazeConstants.WALL, r, c));
 					
-				}else if(line.charAt(j) == MazeConstants.BANANA){
-					B = new Banana(MazeConstants.BANANA,i, j, 
+				}else if(line.charAt(c) == MazeConstants.BANANA){
+					regularB = new Banana(MazeConstants.BANANA,r, c, 
 							MazeConstants.BANANA_SCORE);
-					maze.setCell(i, j, B);
-					bananas.add(B); //add to list of bananas
+					maze.setCell(r, c, regularB);
+					bananas.add(regularB); //add to list of bananas
 
-				}else if(line.charAt(j) == MazeConstants.MOBILE_BANANA){
-					M = new MobileBanana(MazeConstants.MOBILE_BANANA, i, j,
+				}else if(line.charAt(c) == MazeConstants.MOBILE_BANANA){
+					mobileB = new MobileBanana(MazeConstants.MOBILE_BANANA, r, c,
 							MazeConstants.MOBILE_BANANA_SCORE);
-					maze.setCell(i, j, M);
-					bananas.add(M); //add to list of bananas
+					maze.setCell(r, c, mobileB);
+					bananas.add(mobileB); //add to list of bananas
 					
-				}else if(line.charAt(j) == MazeConstants.P1){
-					player1 = new Monkey(MazeConstants.P1, i, j);
-					maze.setCell(i, j, player1);
+				}else if(line.charAt(c) == MazeConstants.P1){
+					player1 = new Monkey(MazeConstants.P1, r, c);
+					maze.setCell(r, c, player1);
 					
-				}else if(line.charAt(j) == MazeConstants.P2){
-					player2 = new Monkey(MazeConstants.P2, i, j);
-					maze.setCell(i, j, player2);
+				}else if(line.charAt(c) == MazeConstants.P2){
+					player2 = new Monkey(MazeConstants.P2, r, c);
+					maze.setCell(r, c, player2);
 					
-				}else if(line.charAt(j) == MazeConstants.VACANT){
-					maze.setCell(i, j, 
-							new UnvisitedHallway(MazeConstants.VACANT, i, j));
+				}else if(line.charAt(c) == MazeConstants.VACANT){
+					maze.setCell(r, c, 
+							new UnvisitedHallway(MazeConstants.VACANT, r, c));
 
 				}
 		}
@@ -163,17 +163,16 @@ public class MazeGame {
 	 */
 	public void move(char nextMove){
 
-		//dimensions for positions of players
-		int p1Row = player1.getRow();
-		int p1Col = player1.getCol();
-		int p2Row = player2.getRow();
-		int p2Col = player2.getCol();
-
 		/*only move if move is valid
 		after each valid move, call moveMobiles method to also move
 		mobile bananas if they exist in maze
 		*/
 		if(validMove(nextMove)){
+			//dimensions for positions of players
+			int p1Row = player1.getRow();
+			int p1Col = player1.getCol();
+			int p2Row = player2.getRow();
+			int p2Col = player2.getCol();
 			if (nextMove == MazeConstants.P1_UP){
 				maze.setCell(p1Row + MazeConstants.UP, p1Col, player1);	
 				maze.setCell(p1Row, p1Col, 
@@ -448,14 +447,11 @@ public class MazeGame {
 	public boolean isBlocked(){
 
 		//uses helper method validMove to check if both players can not move
-		if ((!validMove(MazeConstants.P1_UP)) 
-				&& (!validMove(MazeConstants.P1_RIGHT))
-				&&(!validMove(MazeConstants.P1_DOWN)) 
-				&& (!validMove(MazeConstants.P1_LEFT))
-				&& (!validMove(MazeConstants.P2_UP)) 
-				&& (!validMove(MazeConstants.P2_RIGHT)) 
-				&& (!validMove(MazeConstants.P2_DOWN)) 
-				&& (!validMove(MazeConstants.P2_LEFT))){
+		boolean p1LeftRight = (validMove(MazeConstants.P1_LEFT) && validMove(MazeConstants.P1_RIGHT));
+		boolean p1UpDown = (validMove(MazeConstants.P1_UP) && validMove(MazeConstants.P1_DOWN));
+		boolean p2LeftRight = (validMove(MazeConstants.P2_LEFT) && validMove(MazeConstants.P2_RIGHT));
+		boolean p2UpDown = (validMove(MazeConstants.P2_UP) && validMove(MazeConstants.P2_DOWN));
+		if (!p1LeftRight && !p1UpDown && !p2LeftRight && !p2UpDown){
 			return true;
 		}else{
 			return false;
@@ -471,12 +467,12 @@ public class MazeGame {
 		if (bananas.size() > 0){
 
 			//loop through the list
-			for(int x = 0; x < bananas.size(); x++){
+			for(int b = 0; b < bananas.size(); b++){
 
 				//check if banana at position x is a Mobile Banana
-				if (bananas.get(x).getSymbol() == MazeConstants.MOBILE_BANANA){
-					int mobileRow = bananas.get(x).row;
-					int mobileCol = bananas.get(x).column;
+				if (bananas.get(b).getSymbol() == MazeConstants.MOBILE_BANANA){
+					int mobileRow = bananas.get(b).row;
+					int mobileCol = bananas.get(b).column;
 
 					//4 directions so use random numbers between 0 and 3
 					int randomMove = random.nextInt(4);
@@ -488,54 +484,54 @@ public class MazeGame {
 								mobileCol).getSymbol() 
 								== MazeConstants.VACANT){
 							maze.setCell(mobileRow + MazeConstants.UP, 
-									mobileCol, bananas.get(x));
+									mobileCol, bananas.get(b));
 							maze.setCell(mobileRow, mobileCol, 
 									new UnvisitedHallway(MazeConstants.VACANT, 
 											mobileRow, mobileCol));
-							((MobileBanana) bananas.get(x)).move(mobileRow 
+							((MobileBanana) bananas.get(b)).move(mobileRow 
 									+ MazeConstants.UP, mobileCol);
-					}
+						}
 					}
 					else if(randomMove == 1){
 						if (maze.getCell(mobileRow, mobileCol 
 								+ MazeConstants.RIGHT).getSymbol() 
 								== MazeConstants.VACANT){
 							maze.setCell(mobileRow, mobileCol 
-									+ MazeConstants.RIGHT, bananas.get(x));
-							maze.setCell(bananas.get(x).row, mobileCol, 
+									+ MazeConstants.RIGHT, bananas.get(b));
+							maze.setCell(bananas.get(b).row, mobileCol, 
 									new UnvisitedHallway(MazeConstants.VACANT, 
 											mobileRow, mobileCol));
-							((MobileBanana) bananas.get(x)).move(mobileRow, 
+							((MobileBanana) bananas.get(b)).move(mobileRow, 
 									mobileCol + MazeConstants.RIGHT);
-					}
+						}
 					}else if(randomMove == 2){
 						if (maze.getCell(mobileRow + MazeConstants.DOWN, 
 								mobileCol).getSymbol() == MazeConstants.VACANT){
 							maze.setCell(mobileRow + MazeConstants.DOWN, 
-									mobileCol, bananas.get(x));
+									mobileCol, bananas.get(b));
 							maze.setCell(mobileRow, mobileCol, 
 									new UnvisitedHallway(MazeConstants.VACANT, 
 											mobileRow, mobileCol));
-							((MobileBanana) bananas.get(x)).move(mobileRow 
+							((MobileBanana) bananas.get(b)).move(mobileRow 
 									+ MazeConstants.DOWN, mobileCol);
-					}
+						}
 					}else{
 						if (maze.getCell(mobileRow, mobileCol 
 								+ MazeConstants.LEFT).getSymbol() 
 								== MazeConstants.VACANT){
 							maze.setCell(mobileRow, mobileCol 
-									+ MazeConstants.LEFT, bananas.get(x));
+									+ MazeConstants.LEFT, bananas.get(b));
 							maze.setCell(mobileRow, mobileCol, 
 									new UnvisitedHallway(MazeConstants.VACANT, 
 											mobileRow, mobileCol));
-							((MobileBanana) bananas.get(x)).move(mobileRow, 
+							((MobileBanana) bananas.get(b)).move(mobileRow, 
 									mobileCol + MazeConstants.LEFT);
-					}
+						}
 					
 					}
 					
 				}
 			}
 		}
-		}
+	}
 }
